121bd5f7335e5697e402a174879b92ca6ca57608
Edivânia Pontes <edivaniapts@gmail.com>
2019-06-17 16:39:08 -0300

    conta vagas residencias residentes para pagina do servidor

--- routes/servidor.js
+++ routes/servidor.js
@@ -1,12 +1,38 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")
 const {eAdmin} = require("../helpers/eAdmin")
+
+/* CARREGANDO MODELS */
+require("../models/Residente")
+const Residente = mongoose.model("residentes")
+require("../models/Residencia")
+const Residencia = mongoose.model("residencias")
+require("../models/Quarto")
+const Quarto = mongoose.model("quartos")
 
 /* ROTAS */
 
 /* Home */
-router.get("/", eAdmin, (req, res) => {
-  res.render("servidor/home")
+// router.get("/", eAdmin, (req, res) => {
+//   res.render("servidor/home")
+// })
+
+router.get("/", (req, res) => {
+  //conta residentes
+  Residente.count().then((numResidentes) => {
+    //conta residencias
+    Residencia.count().then((numResidencias) => {
+      //conta vagas
+      Quarto.find().then((quartos) => {
+        var vagas = 0;
+        for (var i = 0; i < quartos.length; i++) {
+          vagas += quartos[i].capacidade - quartos[i].membros
+        }
+        res.render("servidor/home", {numResidentes: numResidentes, numResidencias: numResidencias, vagas: vagas})
+      })
+    })
+  })
 })
 
 /* Gerencia de residencias */
